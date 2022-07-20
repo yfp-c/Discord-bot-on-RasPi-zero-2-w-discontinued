2a22ffce5b3e24fcf049c8d46d3889efb71842ca
Yacob <gfranco@hotmail.co.uk>
2022-07-20 21:28:39 +0100

    updated fortunecookie cog with better code

--- DiscordBot_Current/cogs/fortunecookie.py
+++ DiscordBot_Current/cogs/fortunecookie.py
@@ -8,19 +8,20 @@ class fortunecookie(commands.Cog):
     @commands.command(pass_context = True , aliases=['fc', 'fortune'])
     @commands.cooldown(1, 120, commands.BucketType.user)
     async def fortunecookie(self, ctx):
-        randomnumber1 = random.randint(1,99)
-        randomnumber2 = random.randint(1,99)
-        randomnumber3 = random.randint(1,99)
-        randomnumber4 = random.randint(1,99)
-        randomnumber5 = random.randint(1,99)
-        randomnumber6 = random.randint(1,99)
-        lucky_numbers = f"{randomnumber1}-{randomnumber2}-{randomnumber3}-{randomnumber4}-{randomnumber5}-[{randomnumber6}]"
+        # randomnumber1 = random.randint(1,99)
+        # randomnumber2 = random.randint(1,99)
+        # randomnumber3 = random.randint(1,99)
+        # randomnumber4 = random.randint(1,99)
+        # randomnumber5 = random.randint(1,99)
+        # randomnumber6 = random.randint(1,99)
+        # lucky_numbers = f"{randomnumber1}-{randomnumber2}-{randomnumber3}-{randomnumber4}-{randomnumber5}-[{randomnumber6}]"
+        five_numbers = '-'.join(str(random.randint(1,99)) for _ in range(5))
         with open("./cogs/fortunecookiequotes/fortunecookiequotes.txt", "r") as file:
             allText = file.readlines()
             words = list(map(str, allText))
         await ctx.reply(f"Greetings {ctx.author.name}..."
         f"```fix\n🥠 {random.choice(words)}```"
-        f"```py\n🍀 Your Lucky Numbers: {lucky_numbers}```")
+        f"```py\n🍀 Your Lucky Numbers: {five_numbers}-[{random.randint(1,99)}]```")
 
 
 async def setup(bot):
